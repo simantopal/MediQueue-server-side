77a2c716455dc960241301e740121c10f6b38bc9
simantopal <simantop13@gmail.com>
2026-05-19 12:55:38 +0600

    create tutor page api

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require('express')
-const app = express()
 const dotenv = require("dotenv")
+const cors = require("cors")
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dotenv.config()
 const uri = process.env.MONGODB_URI;
+
+const app = express()
 const PORT = process.env.PORT
+app.use(cors())
+app.use(express.json())
 
 
 const client = new MongoClient(uri, {
@@ -22,6 +26,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const db = client.db("mediqueue")
+    const tutorCollection = db.collection("tutors")
+
+    app.get('/tutor', async(req, res) =>{
+      const result = await tutorCollection.find().toArray();
+      res.json(result)
+    })
+
+    app.post('/tutor', async(req, res) => {
+      const tutorData = req.body
+      console.log(tutorData)
+      const result = await tutorCollection.insertOne(tutorData)
+
+      res.json(result)
+    })
+
+    app.get('/tutor/:id', async(req, res) => {
+      const {id} = req.params
+      const result = await tutorCollection.findOne({_id: new ObjectId(id)})
+
+      res.json(result)
+    })
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
