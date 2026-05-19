42956641e6958d896ebf8fcba2ce1fc9a8650493
simantopal <simantop13@gmail.com>
2026-05-19 21:14:55 +0600

    added booked data in database

--- index.js
+++ index.js
@@ -29,13 +29,14 @@ async function run() {
 
     const db = client.db("mediqueue")
     const tutorCollection = db.collection("tutors")
+    const bookingsCollection = db.collection("bookings");
 
-    app.get('/tutor', async(req, res) =>{
+    app.get('/tutor', async (req, res) => {
       const result = await tutorCollection.find().toArray();
       res.json(result)
     })
 
-    app.post('/tutor', async(req, res) => {
+    app.post('/tutor', async (req, res) => {
       const tutorData = req.body
       console.log(tutorData)
       const result = await tutorCollection.insertOne(tutorData)
@@ -43,14 +44,32 @@ async function run() {
       res.json(result)
     })
 
-    app.get('/tutor/:id', async(req, res) => {
-      const {id} = req.params
-      const result = await tutorCollection.findOne({_id: new ObjectId(id)})
+    app.get('/tutor/:id', async (req, res) => {
+      const { id } = req.params
+      const result = await tutorCollection.findOne({ _id: new ObjectId(id) })
 
       res.json(result)
     })
 
+    app.post('/bookings', async (req, res) => {
+      const bookingData = req.body;
+      console.log(bookingData)
+      const result = await bookingsCollection.insertOne(bookingData);
 
+      res.json(result);
+    });
+
+    app.get('/bookings', async (req, res) => {
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { email: email };
+      }
+
+      const result = await bookingsCollection.find(query).toArray();
+
+      res.json(result);
+    });
 
 
     await client.db("admin").command({ ping: 1 });
@@ -61,10 +80,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) =>{
-    res.send("Server is running fine!")
+app.get('/', (req, res) => {
+  res.send("Server is running fine!")
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
